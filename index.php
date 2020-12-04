2b7e148713c6e79dbc946931eba17118fc89121b
NguyenDung <nguyen_dung@tz.comm>
2020-12-04 14:13:22 +0700

    Dung beo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Omnifood</title>
+    <link rel="stylesheet" type = "stylesheet" href="Vendors/css/normalize.css">
+    <link rel="stylesheet" type = "stylesheet" href="Vendors/css/grid.css">
+    <link rel="stylesheet" type = "text/css" href="resources/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Castoro:ital@1&family=Lato:ital,wght@1,300&display=swap" rel="stylesheet" type = "text/css">
+    
+    
+</head>
+<body>
+    <div class = hero-text-box>
+       <h1>Good by iunk food. Hello super healthy meals</h1>
+    </div>
+    <header>
+
+    </header>
+    
+</body>
+</html>
